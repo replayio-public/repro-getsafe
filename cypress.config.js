8c4159a2a235310c76558e25b3fbc68f544717a8
Jaril <jarilvalenciano@gmail.com>
2023-06-27 09:54:18 -0700

    Configure Replay

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require("cypress");
+const replay = require("@replayio/cypress")
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      replay.default(on, config);
+
+      return config;
       // implement node event listeners here
     },
   },
